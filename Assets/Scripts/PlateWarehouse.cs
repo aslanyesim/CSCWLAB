a0f8114a444e72c3f3f25961521d1f7a358c1bc8
Deniz Bicer <dnzbicer@gmail.com>
2016-01-13 22:50:32 +0100

    Updated gitignore file and json connection
    PlateWarehouse and Printer scripts contain the related script to update
    the tooltip informations.

---
+++ Assets/Scripts/PlateWarehouse.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using LitJson;
+
+public class PlateWarehouse : MonoBehaviour {
+	public string ResourceURL = "http://www.mocky.io/v2/5692924f10000092038be2ea";
+	public Text amountInfo;
+	public float secondsToCheckServer = .1f;
+
+	private JsonData data;
+	private string amount = "";
+
+	void Start () {
+		StartCoroutine(checkForInfo());
+	}
+	
+
+	IEnumerator checkForInfo(){
+		for(;;){
+		WWW www = new WWW(ResourceURL);
+		yield return www;
+		if (www.error == null)
+		{
+			data = JsonMapper.ToObject(www.text);
+			parseData();
+			writeData();
+		}
+		else
+		{
+			Debug.Log("ERROR: " + www.error);
+		}
+			yield return new WaitForSeconds(secondsToCheckServer);
+		}
+	}
+
+	private void parseData(){
+		for (int i = 0; i < data.Count; i++)
+		{ 
+			if(data[i]["name"].ToString() == "plate"){
+				amount = data[i]["amount"].ToString();
+			}
+		}
+	}
+	private void writeData(){
+		amountInfo.text= "Type: Plates\nAmount: " + amount; 
+		data = null;
+	}
+}
---
+++ Assets/Scripts/Printer.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using LitJson;
+
+public class Printer : MonoBehaviour {
+	public string ResourceURL = "http://www.mocky.io/v2/5692924f10000092038be2ea";
+	public string PrinterURL = "http://www.mocky.io/v2/5696324a1300004e36f9e3cd";
+	public Text printerAndBrickInfo;
+	public float secondsToCheckServer = .1f;
+
+	private JsonData printerdata;
+	private JsonData brickdata;
+	private string status = "";
+	private string brickAmount = "";
+
+
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine(checkForInfo());
+	}
+	IEnumerator checkForInfo(){
+		for(;;){
+			WWW www = new WWW(PrinterURL);
+			yield return www;
+			if (www.error == null)
+			{
+				printerdata = JsonMapper.ToObject(www.text);
+				parsePrinterData();
+			}
+			else
+			{
+				Debug.Log("ERROR: " + www.error);
+			}
+
+			www = new WWW(ResourceURL);
+			yield return www;
+			if (www.error == null)
+			{
+				brickdata = JsonMapper.ToObject(www.text);
+				parseBrickData();
+
+			}
+			else
+			{
+				Debug.Log("ERROR: " + www.error);
+			}
+			writeData();
+			yield return new WaitForSeconds(secondsToCheckServer);
+		}
+	}
+	
+	private void parsePrinterData(){
+			status = printerdata["status"].ToString();
+		if(status == "printing"){
+			//CALL PRiNTiNG ANiMATiON
+		} else if (status == "error"){
+			//CALL ERROR ANiMATiON
+		}
+	}
+	private void parseBrickData(){
+		for (int i = 0; i < brickdata.Count; i++)
+		{ 
+			if(brickdata[i]["name"].ToString() == "brick"){
+				brickAmount = brickdata[i]["amount"].ToString();
+			}
+		}
+	}
+	private void writeData(){
+		printerAndBrickInfo.text = "Status: " + status +"\nBrick amount: " + brickAmount;
+		printerdata = null;
+		brickdata = null;
+	}
+}
