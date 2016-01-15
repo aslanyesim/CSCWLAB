5e0e5a4308747a829634ba3c0566add33974c5d6
aslanyesim <aslanyesim@outlook.com>
2016-01-15 11:39:59 +0100

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	Assets/Vuforia/Editor/FrameMarkerTextures/Markers/Thumbs.db.meta
    #	QCAR/somedata16
    #	QCAR/vufsen.dat
    
    Add brick Animation completed
    
    Some useless objects deleted

---
+++ Assets/Scripts/TouchBrick.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TouchBrick : MonoBehaviour {
+
+	public GameObject BrickNew;
+	public GameObject PrinterHead;
+	//private Joint theJoint;
+	public GameObject Cube;
+	public Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+
+			rb = GetComponent<Rigidbody>();
+			
+		}
+
+	void OnCollisionEnter (Collision col){
+				if (col.gameObject.name == "BrickNew") {
+					col.transform.parent = PrinterHead.transform;
+						} 
+				else{
+					Cube.SetActive(true);
+					}
+}
+}
